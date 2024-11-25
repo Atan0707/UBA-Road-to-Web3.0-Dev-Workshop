b693d1145f655e185de2288a1192a919d67ca8a3
Hariz Hakim <harizhakim85@gmail.com>
2024-11-25 18:49:34 +0800

    remove flashbang

--- workshop-1/src/App.jsx
+++ workshop-1/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">
+    <div className="flex items-center justify-center min-h-screen bg-slate-900">
+      <h1 className="text-3xl font-bold text-white">
         Hello World
       </h1>
     </div>
