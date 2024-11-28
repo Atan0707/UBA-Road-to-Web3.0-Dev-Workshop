1968dd4fd61ce305427fd683cec3962c55624af1
Hariz Hakim <harizhakim85@gmail.com>
2024-11-28 22:20:09 +0800

    workshop 28.11

--- workshop-1/src/App.jsx
+++ workshop-1/src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(0);
+let count1 = 1;
+
+function increment () {
+  count1 = count1 + 1;
+  console.log(count1);
+}
+
+const increment2 = () => {
+  setCount(count + 1);
+  console.log(count);
+}
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className=" items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold">
         Hello World
       </h1>
+      <button onClick={increment}>click this</button>
+      <br />
+      <button onClick={increment2}>click this (useste)</button>
+       <p>without useState: {count1}</p>
+      <p>With useState: {count}</p>
     </div>
   );
 }
