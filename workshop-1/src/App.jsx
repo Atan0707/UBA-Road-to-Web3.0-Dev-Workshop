111b2e6d859f2436147afed75b5fec2131244c40
Hariz Hakim <harizhakim85@gmail.com>
2024-11-29 18:04:58 +0800

    update README

--- workshop-1/src/App.jsx
+++ workshop-1/src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'; // this one we call it hook
+// useState is just like normal variable but it will re-render the component when the value is changed
+// useEffect is just like automatic function that will run when the component is rendered
+
 
 function App() {
 const [count, setCount] = useState(0);
 let count1 = 1;
 
-function increment () {
+// this one without react
+function increment (  ) {
   count1 = count1 + 1;
   console.log(count1);
 }
 
+// this one with react
 const increment2 = () => {
   setCount(count + 1);
   console.log(count);
 }
+
+useEffect(() => {
+  console.log("count increased");
+}, [count]);
 
   return (
     <div className=" items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold">
         Hello World
       </h1>
-      <button onClick={increment}>click this</button>
+      <button onClick={increment}>click this</button> 
+      {/* this one without react */}
       <br />
       <button onClick={increment2}>click this (useste)</button>
        <p>without useState: {count1}</p>
