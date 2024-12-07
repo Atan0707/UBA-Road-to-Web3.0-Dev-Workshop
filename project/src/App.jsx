196c6c80dda3cffe59f3638dbee91bf6ae286d4d
Hariz Hakim <harizhakim85@gmail.com>
2024-12-07 21:34:24 +0800

    add routing

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Counter from './pages/Counter'
+import Todolist from './pages/Todolist'
 
 function App() {
-  const [count, setCount] = useState(0) // react hook
-  let counter1 = 0; // tak guna hook
-
-  const handleClick = () => {
-    counter1++;
-    console.log("Without react: ", counter1)
-  }
-
-  const handleClick2 = () => {
-    setCount(count + 1);
-    console.log("With react = ", count);
-  }
 
   return (
-    <div className="app">
-      <button onClick={handleClick}>
-        Tanpa React
-      </button>
-      <button onClick={handleClick2}>
-        With React
-      </button>
-      <p className='text-red-700'>Tanpa React = {counter1}</p>
-      <p>With React = {count}</p>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Counter />} />
+          <Route path='/todolist' element={<Todolist />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
