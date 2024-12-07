6b4b7a9b4accf2f130d2a3cfb31c766dbcf2e367
Hariz Hakim <harizhakim85@gmail.com>
2024-12-07 22:08:54 +0800

    add todo

--- project/src/pages/Todolist.jsx
+++ project/src/pages/Todolist.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Todolist = () => {
+    const [text, setText] = useState("")
+    const [todos, setTodos] = useState([])
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+
+    const handleAdd = (e) => {
+        setTodos([...todos, { text }]);
+        console.log(todos);
+    }
+
   return (
-    <div>Todolist</div>
+    <div>
+        <h1>Todo List</h1>
+        <ul>
+            {todos.map((data, index)=> (
+                <li key={index}>
+                    {data.text}
+                </li>
+            ))}
+        </ul>
+        <input onChange={handleChange} /> 
+        <button onClick={handleAdd}>Add Todo</button>
+    </div>
   )
 }
 
